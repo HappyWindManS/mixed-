53131f241d0250c22b45ce29028e05c7e1842000
仁 蒙 <3066004354@qq.com>
2019-09-11 22:21:00 +0800

    test

---
+++ httpclient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace httpclient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+
+        private async Task GetDataSimpleAsync()
+        {
+            using (var client = new HttpClient())
+            {
+
+            }
+        }
+
+        
+    }
+}
