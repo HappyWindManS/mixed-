0186f110a064835fcd57401a52e3e46a220246f5
仁 蒙 <3066004354@qq.com>
2019-09-17 16:42:59 +0800

    test

---
+++ DataDemo/DataDemo/Test/Program.cs
@@ -0,0 +1,79 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = UTools.ToGB2312("");
+            //Console.Write("\u4e13\u79d1\u6279\uff08CD\uff09");
+            //https://static-data.eol.cn/www/config/detial/1.json
+            Console.ReadKey();
+            Console.Write("https://static-data.eol.cn/www/config/detial/1.json");
+            Console.WriteLine("结束");
+            Console.ReadKey();
+
+            //int local_province_id = 45;
+            //int local_type_id = 1;
+            //int school_id = 102;
+            //int year = 2018;
+            //string p = string.Format(@"api.eol.cn/gkcx/api/?access_token=&local_province_id={0}&local_type_id={1}&school_id={2}&signsafe=&uri=apidata/api/gk/score/special&year={3}",local_province_id,local_type_id,school_id,year);
+            //string url = "https://" + p;
+
+            //Console.WriteLine(GetWebClient(url));
+            //Console.ReadKey();
+        }
+        private static string GetWebClient(string url)
+        {
+            string strHTML = "";
+            WebClient myWebClient = new WebClient();
+            Stream myStream = myWebClient.OpenRead(url);
+            StreamReader sr = new StreamReader(myStream, System.Text.Encoding.GetEncoding("utf-8"));
+            strHTML = sr.ReadToEnd();
+            myStream.Close();
+            return strHTML;
+        }
+
+        private static string GetWebRequest(string url)
+        {
+            Uri uri = new Uri(url);
+            WebRequest myReq = WebRequest.Create(uri);
+            WebResponse result = myReq.GetResponse();
+            Stream receviceStream = result.GetResponseStream();
+            StreamReader readerOfStream = new StreamReader(receviceStream, System.Text.Encoding.GetEncoding("utf-8"));
+            string strHTML = readerOfStream.ReadToEnd();
+            readerOfStream.Close();
+            receviceStream.Close();
+            result.Close();
+            return strHTML;
+        }
+
+        private static string GetHttpWebRequest(string url)
+        {
+            Uri uri = new Uri(url);
+            HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create(uri);
+            myReq.UserAgent = "User-Agent:Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705";
+            myReq.Accept = "*/*";
+            myReq.KeepAlive = true;
+            myReq.Headers.Add("Accept-Language", "zh-cn,en-us;q=0.5");
+            HttpWebResponse result = (HttpWebResponse)myReq.GetResponse();
+            Stream receviceStream = result.GetResponseStream();
+            StreamReader readerOfStream = new StreamReader(receviceStream, System.Text.Encoding.GetEncoding("utf-8"));
+            string strHTML = readerOfStream.ReadToEnd();
+            readerOfStream.Close();
+            receviceStream.Close();
+            result.Close();
+            return strHTML;
+        }
+    }
+}
