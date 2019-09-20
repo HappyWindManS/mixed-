0200fd8a1de43564c64a5302357d89096301ede2
仁 蒙 <3066004354@qq.com>
2019-09-20 22:08:44 +0800

    demo1.7

---
+++ ZGKdata/Data_Table/RegionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZGKdata.Data_Table
+{
+    public class RegionModel
+    {
+        public string region { get; set; }
+        public string id { get; set; }
+    }
+}
---
+++ ZGKdata/Data_Table/SchoolScoreModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZGKdata.Data_Table
+{
+    public class SchoolScoreModel
+    {
+        public string average { get; set; }
+        public string dual_class_name { get; set; }
+        public string filing { get; set; }
+        public string local_batch_name { get; set; }
+        public string local_province_name { get; set; }
+        public string local_type_name { get; set; }
+        public string max { get; set; }
+        public string min { get; set; }
+        public string name { get; set; }
+        public string proscore { get; set; }
+        public string province_id { get; set; }
+        public string school_id { get; set; }
+        public string year { get; set; }
+    }
+}
