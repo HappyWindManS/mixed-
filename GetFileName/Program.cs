9896e60a2e6ab3fda97c4bda0b73b0d030716af0
仁 蒙 <3066004354@qq.com>
2019-09-21 14:29:27 +0800

    test

---
+++ GetFileName/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace GetFileName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\AU";
+            DirectoryInfo root = new DirectoryInfo(path);
+            FileInfo[] files = root.GetFiles();
+            foreach (var item in files)
+            {
+                Console.Write(item.FullName);
+            }
+            Console.ReadKey();
+        }
+    }
+}
