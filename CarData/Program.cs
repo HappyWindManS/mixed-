c0a77b102ed703bb6d84f5ada510d66974bd094d
仁 蒙 <3066004354@qq.com>
2019-09-23 17:45:12 +0800

    CraData

---
+++ CarData/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CarData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //调用解码包
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            //string p = string.Format(@"auto.sohu.com/s2013/60yearpeople/index.shtml"
+            //                         );
+            //var data=GetWebClient("https://" + p);
+            string path = @"C:/Users/ND01/Desktop/新建文本文档.txt";
+            StreamReader sr = new StreamReader(path, Encoding.GetEncoding("GB2312"));
+            String line;
+            string data = "";
+            while ((line = sr.ReadLine()) != null)
+            {
+                data += (line.ToString());
+            }
+
+            Console.WriteLine(StringConformity(data));
+            Console.WriteLine("打印结束");
+            Console.ReadLine();
+        }
+        public static string GetWebClient(string url)
+        {
+            string strHTML = "";
+            WebClient myWebClient = new WebClient();
+            Stream myStream = myWebClient.OpenRead(url);
+            StreamReader sr = new StreamReader(myStream, System.Text.Encoding.GetEncoding("GB2312"));
+            strHTML = sr.ReadToEnd();
+            myStream.Close();
+            return strHTML;
+        }
+        public static string StringConformity(string str)
+        {
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                if (str.Substring(i, 1) == " ")
+                {
+
+                }
+                else if (str.Substring(i, 1) == "年")
+                {
+                    if (str.Substring(i, 4) == "年筑梦人")
+                    {
+                        str = str.Remove(i, 4);
+                        str = str.Insert(i, " ");
+                    }
+                    else if (!CheckStringChineseReg(str.Substring(i, 1)) && !isTel(str.Substring(i, 1)))
+                    {
+                        str = str.Remove(i, 1);
+                        str = str.Insert(i, " ");
+                    }
+                }
+                else if(!CheckStringChineseReg(str.Substring(i, 1)) && !isTel(str.Substring(i, 1)))
+                {
+                    str = str.Remove(i, 1);
+                    str = str.Insert(i, " ");
+                }
+               
+            }
+            return str;
+        }
+        /// <summary>
+        /// 用 正则表达式 判断字符是不是汉字
+        /// </summary>
+        /// <param name="text">待判断字符或字符串</param>
+        /// <returns>真：是汉字；假：不是</returns>
+        public static bool CheckStringChineseReg(string text)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(text, @"[\u4e00-\u9fbb]");
+        }
+        public static bool isTel(string strInput)
+        {
+            return Regex.IsMatch(strInput, @"^[+-]?\d*$");
+        }
+    }
+}
---
+++ TestCarData/TestCarData/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace TestCarData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string url = "";
+            //HttpWebRequest car = (HttpWebRequest)WebRequest.Create(url);
+            //car.KeepAlive = false;
+            //car.Timeout = 30 * 1000;
+            Console.WriteLine(GetWebClient(UrlInfo("", "", "", "")));
+            Console.ReadLine();
+        }
+        //读取网页信息
+        public static string UrlInfo(string local_province_id, string local_type_id, string school_id, string year)
+        {
+            string p = string.Format(@"auto.sohu.com/s2013/60yearpeople/index.shtml");
+            return str(GetWebClient("https://" + p));
+        }
+        //获取的网页信息转换成string
+        public static string GetWebClient(string url)
+        {
+            string strHTML = "";
+            WebClient myWebClient = new WebClient();
+            Stream myStream = myWebClient.OpenRead(url);
+            StreamReader sr = new StreamReader(myStream, System.Text.Encoding.GetEncoding("utf-8"));
+            strHTML = sr.ReadToEnd();
+            myStream.Close();
+            return strHTML;
+        }
+
+        private static string str(string st)
+        {
+            int num = st.IndexOf("[");
+            int num1 = st.IndexOf("]");
+            st = st.Substring(num, num1 - num + 1);
+            return st;
+        }
+    }
+}
